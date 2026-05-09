8acf458d7fcb7ec846dae7d391eef154caa72766
DESKTOP-FKIUNFT\Admin <hisyamadzha@gmail.com>
2026-05-09 16:57:30 +0800

    Added day-12 exercise 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout";
+import ProtectedRoute from "./routes/ProtectedRoute";
 import InstructorListPage from "./pages/InstructorListPage";
 import InstructorDetailPage from "./pages/InstructorDetailPage";
 import LoginPage from "./pages/LoginPage";
@@ -16,7 +17,14 @@ function App() {
         <Route path="/instructors" element={<InstructorListPage />} />
         <Route path="/instructors/:id" element={<InstructorDetailPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar(){
+    const navigate = useNavigate();
+    const token = localStorage.getItem("token");
+    
+    function handleLogout() {
+        localStorage.removeItem("token");
+        navigate("/login");
+    }
+
     return ( 
         <nav className="navbar">
             <div className="nav-brand">Instructor App</div>
@@ -11,7 +19,11 @@ function Navbar(){
                 <span className="nav-link-separator"> | </span>
                 <Link to="/dashboard">Dashboard</Link>
                 <span className="nav-link-separator"> | </span>
-                <Link to="/login">Login</Link>
+                {token ? (
+                    <button onClick={handleLogout}>Logout</button>
+                ) : (
+                    <Link to="/login">Login</Link>
+                )}
             </div>
         </nav>
     );
