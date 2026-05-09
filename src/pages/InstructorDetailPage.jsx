7efe470cd1547e6dff5ef719d1bd8acd8fc2e244
DESKTOP-FKIUNFT\Admin <hisyamadzha@gmail.com>
2026-05-09 12:12:03 +0800

    Added day-12 exercise 3

--- src/pages/InstructorDetailPage.jsx
+++ src/pages/InstructorDetailPage.jsx
@@ -1,8 +1,13 @@
+import { useParams } from "react-router-dom";
+
 function InstructorDetailPage() {
+    const { id } = useParams();
+
     return (
         <section>
             <h1>Instructor Detail Page</h1>
             <p>This is the instructor detail page.</p>
+            <p>ID: {id}</p>
         </section>
     )
 }
